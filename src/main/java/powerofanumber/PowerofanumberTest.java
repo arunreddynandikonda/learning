15da2f73a4417560baf5038119029493086367be
arunreddy <arunreddy186@gmail.com>
2021-05-16 12:48:30 +0530

    akhl

---
+++ src/main/java/powerofanumber/PowerofanumberTest.java
@@ -0,0 +1,9 @@
+package powerofanumber;
+
+public class PowerofanumberTest {
+
+	public static void main(String[] args) {
+		Powerofnumber cal = new Powerofnumber();
+		cal.power(4, 4);
+	}
+}
---
+++ src/main/java/powerofanumber/Powerofnumber.java
@@ -0,0 +1,8 @@
+package powerofanumber;
+
+public class Powerofnumber {
+	public void power(int base, int power) {
+		double result = Math.pow(base, power);
+		System.out.println(" The result is" + result);
+	}
+}
